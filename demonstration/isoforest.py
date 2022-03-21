5d0c4bb7c81db77690b4979393ef58c24830b2d5
cmbfuqua <Benjamin.Fuqua@gmail.com>
2022-03-21 17:23:13 -0600

    updated the other files with the dbscan template

--- demonstration/isoforest.py
+++ demonstration/isoforest.py
@@ -1,4 +1,5 @@
 #%%
+from venv import create
 import pandas as pd 
 import altair as alt
 
@@ -12,25 +13,6 @@ from sklearn import metrics
 # %%
 dat = pd.read_csv('https://raw.githubusercontent.com/byui-cse/cse450-course/master/data/housing.csv')
 # Top 3 features: ['sqft_living','grade','sqft_lot15']
-#%%
-alt.data_transformers.enable(max_rows = None)
-# Create charts for presentation
-sqft = alt.Chart(dat, title = 'Sqft Living Correlation').mark_point().encode(
-    alt.X('price',title = 'Price'),
-    alt.Y('sqft_living', title = 'Square ft Living Space')
-)
-grd = alt.Chart(dat, title = 'Grade Correlation').mark_point().encode(
-    alt.X('price',title = 'Price'),
-    alt.Y('grade', title = 'Grade of House')
-)
-
-lot = alt.Chart(dat,title = 'Sqft Lot Correlation').mark_point().encode(
-    alt.X('price', title = 'Price'),
-    alt.Y('sqft_lot',title = 'Sqft Plot of Land')
-    
-)
-
-normal_chart = sqft|grd|lot
 
 #%% 
 # get rid of non-essential columns
@@ -48,8 +30,8 @@ def clean_house(dat):
 
     # 
     # Categorical values that need to be one-hot encoded
-    # bedrooms, bathrooms, floors, view, condition, grade
-    cat_list = ['bedrooms','bathrooms','floors','view','condition','grade','yr_built_bin']
+    # bedrooms, bathrooms, floors, view, condition
+    cat_list = ['bedrooms','bathrooms','floors','view','condition','yr_built_bin']
     for col in cat_list:
         onehot =  pd.get_dummies(data[col],col, drop_first=True)
         data = pd.concat([data,onehot],axis = 1)
@@ -58,7 +40,7 @@ def clean_house(dat):
     # 
     # Numerical values that need to be scaled
     num_list = ['sqft_living','sqft_lot', 'sqft_above', 'sqft_basement',
-                'sqft_living15', 'sqft_lot15']
+                'sqft_living15', 'sqft_lot15','grade']
     for col in num_list:
         mm = MinMaxScaler(feature_range = (0,10))
         mm.fit(pd.DataFrame(data[col]))
@@ -75,42 +57,76 @@ def clean_house(dat):
 data_train = clean_house(dat)
 # Top 3 features: ['sqft_living','grade','sqft_lot15']
 #%%
+alt.data_transformers.enable(max_rows = None)
+# Create charts for presentation
+sqft = alt.Chart(data_train, title = 'Sqft Living Correlation').mark_point().encode(
+    alt.X('price',title = 'Price'),
+    alt.Y('sqft_living_scaled', title = 'Square ft Living Space')
+)
+grd = alt.Chart(data_train, title = 'Grade Correlation').mark_point().encode(
+    alt.X('price',title = 'Price'),
+    alt.Y('grade_scaled', title = 'Grade of House')
+)
+
+lot = alt.Chart(data_train,title = 'Sqft Lot Correlation').mark_point().encode(
+    alt.X('price', title = 'Price'),
+    alt.Y('sqft_lot_scaled',title = 'Sqft Plot of Land')
+    
+)
+
+normal_chart = sqft|grd|lot
+
 #%%
 ##############################
 # import new models
 ##############################
-
-
+from sklearn.ensemble import IsolationForest as isof
+import numpy as np
 ##################################
 # clean up data with outlier detection model
 ##################################
+detection_model = isof()
+'''
+When allowed to determine the contamination rate, it comes
+out to be around .05. 
+'''
 
+detection_model.fit(data_train)
 
+iso = detection_model.predict(data_train)
 
+iso = pd.DataFrame(iso)
+indicies = iso.loc[iso[0] == 1].index
+data_clean = data_train.iloc[indicies]
+#%%
 ###################################
 # Create new charts
 ###################################
-nsqft = alt.Chart(dat, title = 'Sqft Living Correlation').mark_point().encode(
+nsqft = alt.Chart(data_clean, title = 'Sqft Living Correlation').mark_point().encode(
     alt.X('price',title = 'Price'),
-    alt.Y('sqft_living', title = 'Square ft Living Space')
+    alt.Y('sqft_living_scaled', title = 'Square ft Living Space')
 )
-ngrd = alt.Chart(dat, title = 'Grade Correlation').mark_point().encode(
+ngrd = alt.Chart(data_clean, title = 'Grade Correlation').mark_point().encode(
     alt.X('price',title = 'Price'),
-    alt.Y('grade', title = 'Grade of House')
+    alt.Y('grade_scaled', title = 'Grade of House')
 )
 
-nlot = alt.Chart(dat,title = 'Sqft Lot Correlation').mark_point().encode(
+nlot = alt.Chart(data_clean,title = 'Sqft Lot Correlation').mark_point().encode(
     alt.X('price', title = 'Price'),
-    alt.Y('sqft_lot',title = 'Sqft Plot of Land')
+    alt.Y('sqft_lot_scaled',title = 'Sqft Plot of Land')
     
 )
 
 new_chart = nsqft|ngrd|nlot
 new_chart
+#%%
 # %% This cell runs the model
 # create x & y values
-x = data_train.drop(columns = 'price')
-y = data_train.price
+'''
+Base RMSE without detection is ~220,000
+'''
+x = data_clean.drop(columns = 'price')
+y = data_clean.price
 
 x_train, x_test, y_train, y_test = train_test_split(x,y, test_size= .3, random_state=76)
 
@@ -130,8 +146,130 @@ mae = metrics.mean_absolute_error(y_test,y_pred).round(2)
 print('MSE from data (Sensitive to large prediction error): {}'.format(mse))
 print('MAE from data (Treats all errors equally): {}'.format(mae))
 print('RMSE from data (Actual Units Off): {}'.format(rmse))
+print('Base RMSE 217,240')
+normal_chart & new_chart 
 
+#%%
+def create_effective_charts(data_train,contam):
+    from sklearn.ensemble import IsolationForest
+    import numpy as np
+    ##################################
+    # clean up data with outlier detection model
+    ##################################
+    detection_model = IsolationForest(
+        contamination = contam
+    )
+
+    detection_model.fit(data_train)
+
+    iso = detection_model.predict(data_train)
+
+    iso = pd.DataFrame(iso)
+    indicies = iso.loc[iso[0] == 1].index
+    data_clean = data_train.iloc[indicies]
+
+    #This cell runs the model
+    # create x & y values
+    x = data_clean.drop(columns = 'price')
+    y = data_clean.price
+
+    x_train, x_test, y_train, y_test = train_test_split(x,y, test_size= .3, random_state=76)
+
+    # use default values for knn model
+    knn = KNeighborsRegressor()
+
+    # fit and predict
+    knn.fit(x_train,y_train)
+
+    y_pred = knn.predict(x_test)
+
+    # 
+    rmse = metrics.mean_squared_error(y_test,y_pred,squared=False).round(2)
+
+    return rmse,data_clean.shape[0]
 
 # %%
-new_chart.save('New_chart.png')
+import time
+rmse = []
+contaminate = []
+shape = []
+base = []
+for contam in range(5,35,5):
+    contam = contam/100
+    tic = time.perf_counter() # start timer
+    rmse_loop,dat_shape = create_effective_charts(data_train,contam)
+    # append values to lists
+    contaminate.append(contam)
+    rmse.append(rmse_loop)
+    shape.append(dat_shape)
+    base.append(20000)
+    toc = time.perf_counter() # end timer
+    print('contam: {}  shape: {}  time: {}\n\n'.format(contam,dat_shape,round(toc - tic,2)))
+df_results = pd.DataFrame(
+    {'rmse_values':rmse,
+    'contam':contaminate,
+    'remaining_rows':shape,
+    'start_rows':base}
+)
+# %%
+chart1 = alt.Chart(df_results).mark_line().encode(
+    alt.X('contam',title = 'Contamination %'),
+    alt.Y('rmse_values')
+)
+chart1
 
+# %%
+chart1.save('isoforest_rmse_curve.png')
+# %%
+from sklearn.ensemble import IsolationForest
+detection_model = IsolationForest(
+        contamination = .15
+    )
+
+detection_model.fit(data_train)
+
+iso = detection_model.predict(data_train)
+
+iso = pd.DataFrame(iso)
+indicies = iso.loc[iso[0] == 1].index
+data_clean = data_train.iloc[indicies]
+
+#This cell runs the model
+# create x & y values
+x = data_clean.drop(columns = 'price')
+y = data_clean.price
+
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size= .3, random_state=76)
+
+# use default values for knn model
+knn = KNeighborsRegressor()
+
+# fit and predict
+knn.fit(x_train,y_train)
+
+y_pred = knn.predict(x_test)
+
+# 
+rmse = metrics.mean_squared_error(y_test,y_pred,squared=False).round(2)
+###################################
+# Create new charts
+###################################
+nsqft = alt.Chart(data_clean, title = 'Sqft Living Correlation').mark_point().encode(
+    alt.X('price',title = 'Price'),
+    alt.Y('sqft_living_scaled', title = 'Square ft Living Space')
+)
+ngrd = alt.Chart(data_clean, title = 'Grade Correlation').mark_point().encode(
+    alt.X('price',title = 'Price'),
+    alt.Y('grade_scaled', title = 'Grade of House')
+)
+
+nlot = alt.Chart(data_clean,title = 'Sqft Lot Correlation').mark_point().encode(
+    alt.X('price', title = 'Price'),
+    alt.Y('sqft_lot_scaled',title = 'Sqft Plot of Land')
+    
+)
+
+new_chart = nsqft|ngrd|nlot
+new_chart.save('isolationForest_chart.png')
+new_chart
+# %%
--- demonstration/lof.py
+++ demonstration/lof.py
@@ -1,4 +1,5 @@
 #%%
+from venv import create
 import pandas as pd 
 import altair as alt
 
@@ -12,25 +13,6 @@ from sklearn import metrics
 # %%
 dat = pd.read_csv('https://raw.githubusercontent.com/byui-cse/cse450-course/master/data/housing.csv')
 # Top 3 features: ['sqft_living','grade','sqft_lot15']
-#%%
-alt.data_transformers.enable(max_rows = None)
-# Create charts for presentation
-sqft = alt.Chart(dat, title = 'Sqft Living Correlation').mark_point().encode(
-    alt.X('price',title = 'Price'),
-    alt.Y('sqft_living', title = 'Square ft Living Space')
-)
-grd = alt.Chart(dat, title = 'Grade Correlation').mark_point().encode(
-    alt.X('price',title = 'Price'),
-    alt.Y('grade', title = 'Grade of House')
-)
-
-lot = alt.Chart(dat,title = 'Sqft Lot Correlation').mark_point().encode(
-    alt.X('price', title = 'Price'),
-    alt.Y('sqft_lot',title = 'Sqft Plot of Land')
-    
-)
-
-normal_chart = sqft|grd|lot
 
 #%% 
 # get rid of non-essential columns
@@ -48,8 +30,8 @@ def clean_house(dat):
 
     # 
     # Categorical values that need to be one-hot encoded
-    # bedrooms, bathrooms, floors, view, condition, grade
-    cat_list = ['bedrooms','bathrooms','floors','view','condition','grade','yr_built_bin']
+    # bedrooms, bathrooms, floors, view, condition
+    cat_list = ['bedrooms','bathrooms','floors','view','condition','yr_built_bin']
     for col in cat_list:
         onehot =  pd.get_dummies(data[col],col, drop_first=True)
         data = pd.concat([data,onehot],axis = 1)
@@ -58,7 +40,7 @@ def clean_house(dat):
     # 
     # Numerical values that need to be scaled
     num_list = ['sqft_living','sqft_lot', 'sqft_above', 'sqft_basement',
-                'sqft_living15', 'sqft_lot15']
+                'sqft_living15', 'sqft_lot15','grade']
     for col in num_list:
         mm = MinMaxScaler(feature_range = (0,10))
         mm.fit(pd.DataFrame(data[col]))
@@ -75,42 +57,81 @@ def clean_house(dat):
 data_train = clean_house(dat)
 # Top 3 features: ['sqft_living','grade','sqft_lot15']
 #%%
+alt.data_transformers.enable(max_rows = None)
+# Create charts for presentation
+sqft = alt.Chart(data_train, title = 'Sqft Living Correlation').mark_point().encode(
+    alt.X('price',title = 'Price'),
+    alt.Y('sqft_living_scaled', title = 'Square ft Living Space')
+)
+grd = alt.Chart(data_train, title = 'Grade Correlation').mark_point().encode(
+    alt.X('price',title = 'Price'),
+    alt.Y('grade_scaled', title = 'Grade of House')
+)
+
+lot = alt.Chart(data_train,title = 'Sqft Lot Correlation').mark_point().encode(
+    alt.X('price', title = 'Price'),
+    alt.Y('sqft_lot_scaled',title = 'Sqft Plot of Land')
+    
+)
+
+normal_chart = sqft|grd|lot
+
 #%%
 ##############################
 # import new models
 ##############################
-
-
+from sklearn.cluster import DBSCAN
+import numpy as np
 ##################################
 # clean up data with outlier detection model
 ##################################
+detection_model = DBSCAN(
+    eps = 10,
+    min_samples = 10
+)
 
+db = detection_model.fit(data_train)
 
+core_samples_mask = np.zeros_like(db.labels_, dtype = bool)
+core_samples_mask[db.core_sample_indices_] = True
+labels = db.labels_
 
+n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+n_noise_ = list(labels).count(-1)
+
+print("Estimated number of clusters: %d" % n_clusters_)
+print("Estimated number of noise points: %d" % n_noise_)
+
+data_clean = data_train.iloc[db.core_sample_indices_]
+#%%
 ###################################
 # Create new charts
 ###################################
-nsqft = alt.Chart(dat, title = 'Sqft Living Correlation').mark_point().encode(
+nsqft = alt.Chart(data_clean, title = 'Sqft Living Correlation').mark_point().encode(
     alt.X('price',title = 'Price'),
-    alt.Y('sqft_living', title = 'Square ft Living Space')
+    alt.Y('sqft_living_scaled', title = 'Square ft Living Space')
 )
-ngrd = alt.Chart(dat, title = 'Grade Correlation').mark_point().encode(
+ngrd = alt.Chart(data_clean, title = 'Grade Correlation').mark_point().encode(
     alt.X('price',title = 'Price'),
-    alt.Y('grade', title = 'Grade of House')
+    alt.Y('grade_scaled', title = 'Grade of House')
 )
 
-nlot = alt.Chart(dat,title = 'Sqft Lot Correlation').mark_point().encode(
+nlot = alt.Chart(data_clean,title = 'Sqft Lot Correlation').mark_point().encode(
     alt.X('price', title = 'Price'),
-    alt.Y('sqft_lot',title = 'Sqft Plot of Land')
+    alt.Y('sqft_lot_scaled',title = 'Sqft Plot of Land')
     
 )
 
 new_chart = nsqft|ngrd|nlot
 new_chart
+#%%
 # %% This cell runs the model
 # create x & y values
-x = data_train.drop(columns = 'price')
-y = data_train.price
+'''
+Base RMSE without detection is ~220000
+'''
+x = data_clean.drop(columns = 'price')
+y = data_clean.price
 
 x_train, x_test, y_train, y_test = train_test_split(x,y, test_size= .3, random_state=76)
 
@@ -130,8 +151,129 @@ mae = metrics.mean_absolute_error(y_test,y_pred).round(2)
 print('MSE from data (Sensitive to large prediction error): {}'.format(mse))
 print('MAE from data (Treats all errors equally): {}'.format(mae))
 print('RMSE from data (Actual Units Off): {}'.format(rmse))
+print('Base RMSE 217,240')
+normal_chart & new_chart 
 
+#%%
+def create_effective_charts(data_train,es,mins):
+    from sklearn.cluster import DBSCAN
+    import numpy as np
+    ##################################
+    # clean up data with outlier detection model
+    ##################################
+    detection_model = DBSCAN(
+        eps = es,
+        min_samples = mins
+    )
+
+    db = detection_model.fit(data_train)
+
+    core_samples_mask = np.zeros_like(db.labels_, dtype = bool)
+    core_samples_mask[db.core_sample_indices_] = True
+
+    data_clean = data_train.iloc[db.core_sample_indices_]
+
+    #This cell runs the model
+    # create x & y values
+    x = data_clean.drop(columns = 'price')
+    y = data_clean.price
+
+    x_train, x_test, y_train, y_test = train_test_split(x,y, test_size= .3, random_state=76)
+
+    # use default values for knn model
+    knn = KNeighborsRegressor()
+
+    # fit and predict
+    knn.fit(x_train,y_train)
+
+    y_pred = knn.predict(x_test)
+
+    # 
+    rmse = metrics.mean_squared_error(y_test,y_pred,squared=False).round(2)
+
+    return rmse,data_clean.shape[0]
 
 # %%
-new_chart.save('New_chart.png')
+import time
+rmse = []
+eps = []
+msamples = []
+shape = []
+base = []
+for ep in [1,2,3,7,8,9]:
+    for samp in [6,7,8,9]:
+        tic = time.perf_counter() # start timer
+        rmse_loop,dat_shape = create_effective_charts(data_train,ep,samp)
+        # append values to lists
+        eps.append(ep)
+        msamples.append(samp)
+        rmse.append(rmse_loop)
+        shape.append(dat_shape)
+        base.append(20000)
+        toc = time.perf_counter() # end timer
+        print('eps: {}  sample: {}  time: {}\n\n'.format(ep,samp,round(toc - tic,2)))
+df_results = pd.DataFrame(
+    {'rmse_values':rmse,
+    'eps':eps,
+    'min_samples':msamples,
+    'remaining_rows':shape,
+    'start_rows':base}
+)
+# %%
+chart1 = alt.Chart(df_results).mark_line().encode(
+    alt.X('eps'),
+    alt.Y('rmse_values'),
+    alt.Color('min_samples:N')
+)
+chart1
+# %%
+df_results['percent_remaining'] = df_results.remaining_rows/20000
+chart2 = alt.Chart(df_results).mark_line().encode(
+    alt.X('eps'),
+    alt.Y('percent_remaining'),
+    alt.Color('min_samples:O',sort='descending')
+)
+chart_fin = chart1 | chart2
+# %%
+chart_fin.save('dbscan_percent_remaining.png')
+# %%
+from sklearn.cluster import DBSCAN
+import numpy as np
+##################################
+# clean up data with outlier detection model
+##################################
+# Insert the best eps and min_samples values found above
+eps_best = 8
+min_samples_best = 9
 
+detection_model = DBSCAN(
+    eps = eps_best,
+    min_samples = min_samples_best
+)
+
+db = detection_model.fit(data_train)
+
+data_clean = data_train.iloc[db.core_sample_indices_]
+
+###################################
+# Create new charts
+###################################
+nsqft = alt.Chart(data_clean, title = 'Sqft Living Correlation').mark_point().encode(
+    alt.X('price',title = 'Price'),
+    alt.Y('sqft_living_scaled', title = 'Square ft Living Space')
+)
+ngrd = alt.Chart(data_clean, title = 'Grade Correlation').mark_point().encode(
+    alt.X('price',title = 'Price'),
+    alt.Y('grade_scaled', title = 'Grade of House')
+)
+
+nlot = alt.Chart(data_clean,title = 'Sqft Lot Correlation').mark_point().encode(
+    alt.X('price', title = 'Price'),
+    alt.Y('sqft_lot_scaled',title = 'Sqft Plot of Land')
+    
+)
+
+new_chart = nsqft|ngrd|nlot
+new_chart.save('dbscan_chart.png')
+new_chart
+# %%
